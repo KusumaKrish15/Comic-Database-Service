09bd14f8005f15530d5ad899ecbe851d6d164b12
KusumaKrish15 <kusuma.anand@gmail.com>
2025-06-23 21:13:35 -0700

    delete docusaurus project files

---
+++ my-website/.docusaurus/client-modules.js
@@ -0,0 +1,7 @@
+export default [
+  require("/Users/k0k06t6/Documents/Comic-Database-Service/my-website/.docusaurus/docusaurus-plugin-css-cascade-layers/default/layers.css"),
+  require("/Users/k0k06t6/Documents/Comic-Database-Service/my-website/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/k0k06t6/Documents/Comic-Database-Service/my-website/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
+  require("/Users/k0k06t6/Documents/Comic-Database-Service/my-website/node_modules/@docusaurus/theme-classic/lib/nprogress"),
+  require("/Users/k0k06t6/Documents/Comic-Database-Service/my-website/src/css/custom.css"),
+];
